1f27f430efd25d1ef023f3d0069dc51c0832f723
Miklós Richárd <mricsi97@gmail.com>
2020-04-30 20:11:00 +0200

    basic auth communication and first counter communication done

--- app/src/main/java/hu/votingclient/MainActivity.java
+++ app/src/main/java/hu/votingclient/MainActivity.java
@@ -2,43 +2,35 @@ package hu.votingclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
-import org.spongycastle.crypto.Commitment;
-import org.spongycastle.crypto.Committer;
-import org.spongycastle.crypto.commitments.GeneralHashCommitter;
-import org.spongycastle.crypto.digests.SHA256Digest;
-import org.spongycastle.crypto.engines.RSABlindingEngine;
-import org.spongycastle.crypto.generators.RSABlindingFactorGenerator;
-import org.spongycastle.crypto.params.RSABlindingParameters;
-import org.spongycastle.crypto.params.RSAKeyParameters;
-import org.spongycastle.jcajce.provider.symmetric.ARC4;
+import org.bouncycastle.crypto.Commitment;
+import org.bouncycastle.crypto.Committer;
+import org.bouncycastle.crypto.CryptoException;
+import org.bouncycastle.crypto.commitments.GeneralHashCommitter;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.engines.RSABlindingEngine;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.generators.RSABlindingFactorGenerator;
+import org.bouncycastle.crypto.params.RSABlindingParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.signers.PSSSigner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.math.BigInteger;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.Security;
-import java.security.Signature;
-import java.security.SignatureException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
@@ -49,11 +41,15 @@ import java.security.spec.X509EncodedKeySpec;
 
 public class MainActivity extends AppCompatActivity {
 
-    static { Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1); }
+    //static { Security.insertProviderAt(new org.bouncycastle.jce.provider.BouncyCastleProvider(), 1); }
+    //static { Security.addProvider(new BouncyCastleProvider()); }
 
     private static final String TAG = "MainActivity";
 
-    // RSA key strings
+    private static int saltLength = 20;
+
+    // RSA keys
+    private RSAPrivateKey ownPrivateSignatureKey;
     private static final String ownPrivateSignatureKeyString =
             "-----BEGIN PRIVATE KEY-----\n" +
             "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAI8gtRtJsw+fOURa\n" +
@@ -71,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
             "WyY4KN0F72ncHaDnYrhzeCCWU6OcMSrfOt7lqBEgn3WRrinr2g9FVFBjSDTRXRsn\n" +
             "7p+V7PIQKuO73A==\n" +
             "-----END PRIVATE KEY-----";
+
+    private RSAPublicKey ownPublicSignatureKey;
     private static final String ownPublicSignatureKeyString =
             "-----BEGIN PUBLIC KEY-----\n" +
             "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPILUbSbMPnzlEWskxKYPD4rvP\n" +
@@ -78,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
             "Np+7xD4+XqmcUF8GRGf8/ZN/O1tB4UOpEIZ5wLnk0LqXQR12sZz412WdhAqoWq5g\n" +
             "41yOFCSAwdnU9PdqXwIDAQAB\n" +
             "-----END PUBLIC KEY-----";
+
+    private RSAPublicKey counterLongTermPublicKey;
     private static final String counterLongTermPublicKeyString =
             "-----BEGIN PUBLIC KEY-----\n" +
             "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCs/wMsJJexMSG3klBT+Fu7Tgyr\n" +
@@ -85,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
             "9jq3oe5dcwzk7AlWJQ/9HRPJrYqJLfLHJczMEGokdDvFEXCW1siNUZoh7SIn/0iF\n" +
             "e6vXEbQ0mf86gIYugwIDAQAB\n" +
             "-----END PUBLIC KEY-----";
+
+    private RSAPublicKey authorityPublicBlindingKey;
     private static final String authorityPublicBlindingKeyString =
             "-----BEGIN PUBLIC KEY-----\n" +
             "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgF8hrv+1z1yMJA6UFZ5J/uFQ+Xp9\n" +
@@ -93,18 +95,16 @@ public class MainActivity extends AppCompatActivity {
             "Nmf8GXzGZp1AgGgdAgMBAAE=\n" +
             "-----END PUBLIC KEY-----";
 
-    // Always 8 digits
-    private static final Integer myID = 12345678;
+    private static final Integer myID = 12345678; // always 8 digits
     private static final String serverIp = "192.168.8.100";
-
-    // Actual RSA keys
-    private RSAPrivateKey ownPrivateSignatureKey;
-    private RSAPublicKey ownPublicSignatureKey;
-    private RSAPublicKey counterLongTermPublicKey;
-    private RSAPublicKey authorityPublicBlindingKey;
+    private static final int authorityPort = 6868;
+    private static final int counterPort = 6869;
 
     private BigInteger blindingFactor;
     private Commitment commitment;
+    private byte[] signedCommitment;
+
+    private Integer voteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,12 +114,16 @@ public class MainActivity extends AppCompatActivity {
         createKeyObjectsFromStrings();
 
         // Test data for chosen vote option
-        String voteString = "fefheaohfoahfefh";
+        String voteString = "Trump";
+        Log.i(TAG, "Vote: " + voteString);
         byte[] voteBytes = voteString.getBytes(StandardCharsets.UTF_8);
 
         commitment = commitVote(voteBytes);
+        Log.i(TAG, "Commitment: " + Base64.encodeToString(commitment.getCommitment(), Base64.NO_WRAP));
         byte[] blindedCommitment = blindCommitment();
-        byte[] signedBlindedCommitment = signSHA256withRSA(blindedCommitment);
+        Log.i(TAG, "Blinded commitment: " + Base64.encodeToString(blindedCommitment, Base64.NO_WRAP));
+        byte[] signedBlindedCommitment = signSHA256withRSAandPSS(ownPrivateSignatureKey, blindedCommitment);
+        Log.i(TAG, "Signature of blinded commitment: " + Base64.encodeToString(signedBlindedCommitment, Base64.NO_WRAP));
 
         sendToAuthority(blindedCommitment, signedBlindedCommitment);
 
@@ -239,67 +243,174 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private byte[] blindCommitment(){
+        RSABlindingEngine blindingEngine = new RSABlindingEngine();
         RSAKeyParameters keyParameters = new RSAKeyParameters(false, authorityPublicBlindingKey.getModulus(), authorityPublicBlindingKey.getPublicExponent());
+
         RSABlindingFactorGenerator blindingFactorGenerator = new RSABlindingFactorGenerator();
         blindingFactorGenerator.init(keyParameters);
         blindingFactor = blindingFactorGenerator.generateBlindingFactor();
-        RSABlindingEngine rsaBlindingEngine = new RSABlindingEngine();
-        rsaBlindingEngine.init(true, new RSABlindingParameters(keyParameters, blindingFactor));
+        Log.i(TAG, "Blinding factor: " + blindingFactor.toString());
+
+        RSABlindingParameters blindingParameters = new RSABlindingParameters(keyParameters, blindingFactor);
+
         byte[] commitmentBytes = commitment.getCommitment();
 
-        return rsaBlindingEngine.processBlock(commitmentBytes, 0, commitmentBytes.length);
-    }
+        PSSSigner blindSigner = new PSSSigner(blindingEngine, new SHA256Digest(), 20);
+        blindSigner.init(true, blindingParameters);
+        blindSigner.update(commitmentBytes, 0, commitmentBytes.length);
 
-    private byte[] signSHA256withRSA(byte[] message) {
-        Signature sig;
+        //blindingEngine.init(true, blindingParameters);
+        //return blindingEngine.processBlock(commitmentBytes, 0, commitmentBytes.length);
+
+        byte[] blinded = null;
         try {
-            sig = Signature.getInstance("SHA256withRSA");
-            sig.initSign(ownPrivateSignatureKey);
-            sig.update(message);
-            return sig.sign();
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            blinded = blindSigner.generateSignature();
+        } catch (CryptoException e) {
             e.printStackTrace();
-            Log.e(TAG, "Signing failed");
         }
-        return null;
+        return blinded;
     }
 
     private void sendToAuthority(byte[] blindedCommitment, byte[] signedBlindedCommitment){
         new AuthorityCommunication().execute(blindedCommitment, signedBlindedCommitment);
     }
 
-    private class AuthorityCommunication extends AsyncTask<Object, Void, Void> {
+    private class AuthorityCommunication extends AsyncTask<Object, Void, Boolean> {
 
         @Override
-        protected Void doInBackground(Object... objects) {
+        protected Boolean doInBackground(Object... objects) {
             if(android.os.Debug.isDebuggerConnected())
                 android.os.Debug.waitForDebugger();
             byte[] blindedCommitment = (byte[]) objects[0];
             byte[] signedBlindedCommitment = (byte[]) objects[1];
 
-            try ( Socket socket = new Socket(serverIp, 6868);
-                  PrintWriter pw = new PrintWriter(socket.getOutputStream()) ) {
-                Log.i(TAG, myID.toString() + Base64.encodeToString(blindedCommitment, Base64.NO_WRAP) + Base64.encodeToString(signedBlindedCommitment, Base64.NO_WRAP));
-                pw.println(myID.toString() + Base64.encodeToString(blindedCommitment, Base64.NO_WRAP) + Base64.encodeToString(signedBlindedCommitment, Base64.NO_WRAP));
+            Log.i(TAG,"Connecting to authority...");
+            try (Socket socket = new Socket(serverIp, authorityPort)) {
+                Log.i(TAG, "Connected successfully");
+
+                PrintWriter out = null;
+                try {
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                    Log.i(TAG, "Sending to authority...");
+                    out.println(myID.toString() + Base64.encodeToString(blindedCommitment, Base64.NO_WRAP) + Base64.encodeToString(signedBlindedCommitment, Base64.NO_WRAP));
+                    Log.i(TAG, "Data sent");
+                } catch (IOException e) {
+                    Log.e(TAG, "Failed sending data to authority.");
+                    e.printStackTrace();
+                } finally {
+                    socket.shutdownOutput();
+                }
+
+                String authSignedBlindedCommitmentString = null;
+                try ( InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                      BufferedReader in = new BufferedReader(isr) ) {
+                    Log.i(TAG, "Waiting for data...");
+                    authSignedBlindedCommitmentString = in.readLine();
+                    Log.i(TAG, "Received data");
+                } catch (IOException e) {
+                    System.err.println("Failed receiving data from authority.");
+                    e.printStackTrace();
+                }
+                if(authSignedBlindedCommitmentString == null){
+                    Log.i(TAG, "Received data invalid.");
+                    return false;
+                }
+
+                Log.i(TAG, "Blinded commitment signed by the authority: " + authSignedBlindedCommitmentString);
+                byte[] authSignedBlindedCommitment = Base64.decode(authSignedBlindedCommitmentString, Base64.NO_WRAP);
+                signedCommitment = unblindCommitment(authSignedBlindedCommitment);
+                Log.i(TAG, "Commitment signed by authority: " + Base64.encodeToString(signedCommitment, Base64.NO_WRAP));
+
             } catch (IOException e) {
+                Log.e(TAG, "Failed connecting to the authority with the given IP address and port.");
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if(success)
+                sendToCounter(commitment.getCommitment(), signedCommitment);
+        }
+    }
+
+    private void sendToCounter(byte[] commitment, byte[] signature){
+        new CounterCommunication().execute(commitment, signature);
+    }
+
+    private class CounterCommunication extends AsyncTask<Object, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Object... objects) {
+            if(android.os.Debug.isDebuggerConnected())
+                android.os.Debug.waitForDebugger();
+            byte[] commitment = (byte[]) objects[0];
+            byte[] signature = (byte[]) objects[1];
+
+            Log.i(TAG,"Connecting to counter...");
+            try (Socket socket = new Socket(serverIp, counterPort)) {
+                Log.i(TAG, "Connected successfully");
+
+                PrintWriter out = null;
+                try {
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                    Log.i(TAG, "Sending to counter...");
+                    out.println(Base64.encodeToString(commitment, Base64.NO_WRAP) + Base64.encodeToString(signature, Base64.NO_WRAP));
+                    Log.i(TAG, "Data sent");
+                } catch (IOException e) {
+                    Log.e(TAG, "Failed sending data to the counter.");
+                    e.printStackTrace();
+                } finally {
+                    socket.shutdownOutput();
+                }
+
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    Log.i(TAG, "Waiting for data...");
+                    String voteIdString = in.readLine();
+                    Log.i(TAG, "Received data");
+                    if(voteIdString == null){
+                        Log.i(TAG, "Received invalid data from the counter.");
+                        return null;
+                    }
+                    voteId = Integer.parseInt(voteIdString);
+                    Log.i(TAG, "Vote identifier: " + voteId);
+                } catch (IOException e) {
+                    Log.e(TAG, "Failed receiving data from the counter.");
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Failed connecting to the counter with the given IP address and port.");
                 e.printStackTrace();
             }
             return null;
         }
-
-        @Override
-        protected void onPreExecute() {
-            Toast.makeText(MainActivity.this, "pre", Toast.LENGTH_LONG).show();
-            Log.i(TAG, "pre");
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Toast.makeText(MainActivity.this, "post", Toast.LENGTH_LONG).show();
-            Log.i(TAG, "post");
-        }
-
     }
+
+    private byte[] unblindCommitment(byte[] blindedCommitment){
+        RSABlindingEngine rsaBlindingEngine = new RSABlindingEngine();
+        RSAKeyParameters keyParameters = new RSAKeyParameters(false, authorityPublicBlindingKey.getModulus(), authorityPublicBlindingKey.getPublicExponent());
+        RSABlindingParameters blindingParameters = new RSABlindingParameters(keyParameters, blindingFactor);
+
+        rsaBlindingEngine.init(false, blindingParameters);
+
+        return rsaBlindingEngine.processBlock(blindedCommitment, 0, blindedCommitment.length);
+    }
+
+    private byte[] signSHA256withRSAandPSS(RSAPrivateKey signingKey, byte[] message){
+        RSAKeyParameters keyParameters = new RSAKeyParameters(true, signingKey.getModulus(), signingKey.getPrivateExponent());
+
+        PSSSigner signer = new PSSSigner(new RSAEngine(), new SHA256Digest(), saltLength);
+        signer.init(true, keyParameters);
+        signer.update(message, 0, message.length);
+
+        byte[] signature = null;
+        try {
+            signature = signer.generateSignature();
+        } catch (CryptoException e) {
+            e.printStackTrace();
+        }
+        return signature;
+    }
+
 }
